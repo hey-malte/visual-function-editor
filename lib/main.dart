961efa561805d507a1bb1898e19f60c3a7f22fd0
Malte Großmann <malte@hey-malte.de>
2025-11-28 19:11:13 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const VisualFunctionEditorApp());
+}
+
+class VisualFunctionEditorApp extends StatelessWidget {
+  const VisualFunctionEditorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
